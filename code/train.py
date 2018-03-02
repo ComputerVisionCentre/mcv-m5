909fb91402c3cab56c70d8330e093c808d967edd
lluisgomez <lgomez@cvc.uab.es>
2018-03-02 12:20:47 +0100

    allows testing the model even when no training/validation is required

--- code/train.py
+++ code/train.py
@@ -42,7 +42,7 @@ def process(cf):
 
     if cf.test_model:
         # Compute validation metrics
-        model.test(valid_gen)
+        if (valid_gen != None): model.test(valid_gen)
         # Compute test metrics
         model.test(test_gen)
 
